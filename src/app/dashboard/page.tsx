6561bb947f7bde70ffa85efd7384f9adb24a84b4
Marvin Villamar <villamar.marvin.b.8138gmail.com@Marvins-MacBook-Pro.local>
2025-02-22 18:25:09 +0800

    [UPD] responsive

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Sidebar from "@/components/Sidebar/sidebar";
 import About from "@/components/MainContent/about";
-import Navigator from "@/components/Navigator/navigator";
 
 const Dashboard: React.FC = () => {
   return (
     <main className="page-container">
       <Sidebar />
       <About />
-      <Navigator />
     </main>
   );
 };
--- src/components/MainContent/about.tsx
+++ src/components/MainContent/about.tsx
@@ -1,17 +1,22 @@
 import { FileCode2, FileJson2, SquareCode } from "lucide-react";
+import Image from "next/image";
+import Navigator from "../Navigator/navigator";
 
 const About: React.FC = () => {
   return (
     <div className="main-content common-main">
+      <Navigator />
       <div className="skills about-me">
         <h1>About Me</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-        molestiae tempore, doloribus culpa voluptas commodi inventore magnam
-        minima, quia recusandae dignissimos explicabo ullam deserunt optio
-        veritatis animi nostrum ducimus enim.
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          molestiae tempore, doloribus culpa voluptas commodi inventore magnam
+          minima, quia recusandae dignissimos explicabo ullam deserunt optio
+          veritatis animi nostrum ducimus enim.
+        </p>
       </div>
       <div className="skills can-do">
-        <h2>What I can do</h2>
+        <h1>What I can do</h1>
         <div className="skills-container">
           <div className="icon">
             <FileCode2 size={35} />
@@ -19,10 +24,8 @@ const About: React.FC = () => {
           <div className="description">
             <h4>Frontend Development</h4>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Repellendus aspernatur ipsum, esse at recusandae corrupti
-              veritatis, laborum dolore explicabo, magnam eaque. Dolorum,
-              dolorem nemo harum dolore sequi tenetur eius dignissimos.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Adipisci, dolor quis temporibus odio aliquid praesentium iure!
             </p>
           </div>
         </div>
@@ -55,61 +58,173 @@ const About: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="skills softskills">
-        <h2>My Soft Skills</h2>
-        <ul className="softskills-container">
-          <li>
-            <div className="skills-card">
-              <h4>Title</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-                quis perferendis, ipsum adipisci assumenda consequatur, aut
-                consectetur illum sunt facilis odio ea ullam itaque repellendus
-                quod laboriosam unde tempore! Totam.
-              </p>
-            </div>
-          </li>
-          <li>
-            <div className="skills-card">
-              <h4>Title</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-                quis perferendis, ipsum adipisci assumenda consequatur, aut
-                consectetur illum sunt facilis odio ea ullam itaque repellendus
-                quod laboriosam unde tempore! Totam.
-              </p>
-            </div>
-          </li>
-          <li>
-            <div className="skills-card">
-              <h4>Title</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-                quis perferendis, ipsum adipisci assumenda consequatur, aut
-                consectetur illum sunt facilis odio ea ullam itaque repellendus
-                quod laboriosam unde tempore! Totam.
-              </p>
-            </div>
-          </li>
-          <li>
-            <div className="skills-card">
-              <h4>Title</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-                quis perferendis, ipsum adipisci assumenda consequatur, aut
-                consectetur illum sunt facilis odio ea ullam itaque repellendus
-                quod laboriosam unde tempore! Totam.
-              </p>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div className="skills about-me">
-        <h2>What I'm doing</h2>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-        molestiae tempore, doloribus culpa voluptas commodi inventore magnam
-        minima, quia recusandae dignissimos explicabo ullam deserunt optio
-        veritatis animi nostrum ducimus enim.
+      <div className="skills tech-stack">
+        <h1>Technology Experience</h1>
+        <div className="rounded-2xl border border-[var(--onyx)] mt-2 py-3">
+          <h2>Backend Skills</h2>
+          <ul className="tech-list">
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg"
+                  alt="java"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg"
+                  alt="nodejs"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nestjs/nestjs-original-wordmark.svg"
+                  alt="nestjs"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original-wordmark.svg"
+                  alt="spring"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/djangorest/djangorest-original.svg"
+                  alt="rest"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="rounded-2xl border border-[var(--onyx)] mt-2 py-3">
+          <h2>Frontend Skills</h2>
+          <ul className="tech-list">
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg"
+                  alt="react"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-line-wordmark.svg"
+                  alt="vuejs"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+                  alt="javascript"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
+                  alt="typescript"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+                  alt="html"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
+                  alt="css"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original-wordmark.svg"
+                  alt="tailwind"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="rounded-2xl border border-[var(--onyx)] mt-2 py-3">
+          <h2>Database</h2>
+          <ul className="tech-list">
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg"
+                  alt="mysql"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/hibernate/hibernate-original.svg"
+                  alt="hibernate"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+            <li>
+              <div className="tech-card">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"
+                  alt="mongodb"
+                  width={50}
+                  height={50}
+                />
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
